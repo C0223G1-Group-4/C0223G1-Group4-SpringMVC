12534c75175d9f1e505f7fe9acaeab8afb87d20f
phuoctai0612 <sieuem2002@gmail.com>
2023-07-12 02:15:27 +0700

    tai commit l 24

--- case_study/src/main/java/com/example/case_study/repository/IFlightScheduleAirCraftRepository.java
+++ case_study/src/main/java/com/example/case_study/repository/IFlightScheduleAirCraftRepository.java
@@ -14,36 +14,63 @@ public interface IFlightScheduleAirCraftRepository extends JpaRepository< Flight
 
     Page<FlightScheduleAirCraft> getFlightScheduleAirCraftsByFlagIsFalse(Pageable pageable);
     List<FlightScheduleAirCraft> getFlightScheduleAirCraftById(int id);
+
+    @Query(value = "SELECT * FROM flight_schedule_aircraft fsa \n" +
+            "JOIN flight_schedule fs ON fsa.flight_schedule_id=fs.id \n" +
+            "JOIN air_craft ac ON fsa.air_craft_id=ac.id \n" +
+            "JOIN route_air_craft rac ON ac.id=rac.air_craft_id \n" +
+            "JOIN route r ON rac.route_id=r.id \n" +
+            "WHERE fs.arrival LIKE CONCAT('%',:arrival, '%') \n" +
+            "AND fs.departure >= CURDATE() " +
+            "AND MONTH(fs.departure) >= MONTH(current_date()) \n" +
+            "AND YEAR(fs.departure) = YEAR(current_date()) \n" +
+            "AND r.destination LIKE CONCAT('%', :destination, '%') \n" +
+            "AND r.air_port LIKE CONCAT('%', :airPort, '%') \n" +
+            "AND ac.flag=false AND r.flag=false AND fs.flag=false ",nativeQuery = true)
+    List<FlightScheduleAirCraft> searchTicket(@Param("airPort")String airPort
+            ,@Param("arrival") String arrival,@Param("destination") String destination);
+
+
+
+
+    @Query(value = "  SELECT * FROM flight_schedule_aircraft fsa JOIN flight_schedule fs ON fsa.flight_schedule_id = fs.id\n" +
+            " JOIN air_craft ac ON fsa.air_craft_id = ac.id JOIN route_air_craft rac ON ac.id = rac.air_craft_id \n" +
+            " JOIN route r ON rac.route_id = r.id \n" +
+            "WHERE fs.arrival BETWEEN NOW() AND  :arrival" +
+            " and fs.departure > CURDATE() " +
+            " and r.destination like concat('%', :destination, '%') and r.air_port like concat('%',:airPort, '%') \n" +
+            "AND ac.flag = FALSE AND r.flag = FALSE AND fs.flag = FALSE",nativeQuery = true)
+    List<FlightScheduleAirCraft> searchTicket1(@Param("airPort")String airPort,@Param("arrival") String arrival,@Param("destination") String destination );
+
+
     @Query(value = "select * from flight_schedule_aircraft fsa join flight_schedule fs " +
             "on fsa.flight_schedule_id=fs.id join air_craft ac on fsa.air_craft_id=ac.id " +
             "join route_air_craft rac on ac.id=rac.air_craft_id join route r on rac.route_id=r.id where  fs.arrival " +
-            "like concat('%',:arrival,'%') and day(fs.departure) >=  day(current_date()) and r.destination" +
-            " like concat('%',:destination,'%') and r.air_port like concat('%',:airPort,'%')" +
-            "and ac.flag=false and r.flag=false and fs.flag=false ",nativeQuery = true)
-    List<FlightScheduleAirCraft> searchTicket(@Param("airPort")String airPort
-            ,@Param("arrival") String arrival,@Param("destination") String destination);
-      @Query(value = "select * from flight_schedule_aircraft fsa join flight_schedule fs " +
-            "on fsa.flight_schedule_id=fs.id join air_craft ac on fsa.air_craft_id=ac.id " +
-            "join route_air_craft rac on ac.id=rac.air_craft_id join route r on rac.route_id=r.id where  " +
-            " day(fs.arrival) <= day(:arrival) and day(fs.departure) >= day(current_date()) and r.destination" +
-            " like concat('%',:destination,'%') and r.air_port like concat('%',:airPort,'%')" +
-            "and ac.flag=false and r.flag=false and fs.flag=false ",nativeQuery = true)
-    List<FlightScheduleAirCraft> searchTicket1(@Param("airPort")String airPort
-            ,@Param("arrival") String arrival,@Param("destination") String destination);
-      @Query(value = "select * from flight_schedule_aircraft fsa join flight_schedule fs " +
-            "on fsa.flight_schedule_id=fs.id join air_craft ac on fsa.air_craft_id=ac.id " +
-            "join route_air_craft rac on ac.id=rac.air_craft_id join route r on rac.route_id=r.id where  fs.arrival " +
-            "like concat('%',:arrival,'%') and day(fs.departure) >= day(:departure) and r.destination " +
-            " like concat('%',:destination,'%') and r.air_port like concat('%',:airPort,'%')" +
+            "like concat('%',:arrival, '%') and STR_TO_DATE(fs.departure, '%Y-%m-%d') > STR_TO_DATE(:departure, '%Y-%m-%d') and month(fs.departure) >= month(current_date()) and year(fs.departure)=year(current_date())" +
+            " and r.destination like concat('%', :destination, '%') and r.air_port like concat('%',:airPort, '%')" +
             "and ac.flag=false and r.flag=false and fs.flag=false ",nativeQuery = true)
     List<FlightScheduleAirCraft> searchTicket2(@Param("airPort")String airPort,@Param("departure") String departure
             ,@Param("arrival") String arrival,@Param("destination") String destination);
-@Query(value = "select * from flight_schedule_aircraft fsa join flight_schedule fs " +
+           @Query(value = "select * from flight_schedule_aircraft fsa join flight_schedule fs " +
             "on fsa.flight_schedule_id=fs.id join air_craft ac on fsa.air_craft_id=ac.id " +
-            "join route_air_craft rac on ac.id=rac.air_craft_id join route r on rac.route_id=r.id where  day(fs.arrival) " +
-            " <= day(:arrival) and day(fs.departure) >= day(:departure) and r.destination " +
-            " like concat('%',:destination,'%') and r.air_port like concat('%',:airPort,'%')" +
+            "join route_air_craft rac on ac.id=rac.air_craft_id join route r on rac.route_id=r.id where  fs.arrival " +
+            "like concat('%', :arrival,'%') and fs.departure >= current_date() and month(fs.departure) >= month(current_date()) and year(fs.departure)=year(current_date()) and r.destination " +
+            " like concat('%', :destination,'%') and r.air_port like concat('%', :airPort,'%')" +
             "and ac.flag=false and r.flag=false and fs.flag=false ",nativeQuery = true)
-    List<FlightScheduleAirCraft> searchTicket3(@Param("airPort")String airPort,@Param("departure") String departure
+    List<FlightScheduleAirCraft> searchTicket4(@Param("airPort")String airPort
             ,@Param("arrival") String arrival,@Param("destination") String destination);
+
+
+    @Query(value = "SELECT * FROM flight_schedule_aircraft fsa\n" +
+            "JOIN flight_schedule fs ON fsa.flight_schedule_id = fs.id\n" +
+            "JOIN air_craft ac ON fsa.air_craft_id = ac.id\n" +
+            "JOIN route_air_craft rac ON ac.id = rac.air_craft_id \n" +
+            "JOIN route r ON rac.route_id = r.id \n" +
+            " WHERE fs.arrival BETWEEN :departure AND :arrival \n" +
+            " AND fs.departure BETWEEN :departure AND :arrival" +
+            " and r.destination like concat('%',:destination,'%') and r.air_port like concat('%',:airPort,'%') \n" +
+            "AND ac.flag = FALSE AND r.flag = FALSE AND fs.flag = FALSE ",nativeQuery = true)
+        List<FlightScheduleAirCraft> searchTicket3(@Param("airPort")String airPort,@Param("departure") String departure
+                ,@Param("arrival") String arrival,@Param("destination") String destination);
+
 }
--- case_study/src/main/java/com/example/case_study/service/flight_scgedule_air_craft_service/FlightScheduleAirCraftService.java
+++ case_study/src/main/java/com/example/case_study/service/flight_scgedule_air_craft_service/FlightScheduleAirCraftService.java
@@ -7,10 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Service
-public class FlightScheduleAirCraftService implements IFlightScheduleAirCraftService{
+public class FlightScheduleAirCraftService implements IFlightScheduleAirCraftService {
     @Autowired
     private IFlightScheduleAirCraftRepository iFlightScheduleAirCraftRepository;
 
@@ -26,7 +30,7 @@ public class FlightScheduleAirCraftService implements IFlightScheduleAirCraftSer
 
     @Override
     public void createFlightScheduleAirCraft(FlightScheduleAirCraft flightScheduleAirCraft) {
-             iFlightScheduleAirCraftRepository.save(flightScheduleAirCraft);
+        iFlightScheduleAirCraftRepository.save(flightScheduleAirCraft);
     }
 
     @Override
@@ -45,14 +49,23 @@ public class FlightScheduleAirCraftService implements IFlightScheduleAirCraftSer
     }
 
     @Override
-    public List<FlightScheduleAirCraft> searchTicket(String airPort,String departure, String arrival, String destination) {
+    public List<FlightScheduleAirCraft> searchTicket(String airPort, String departure, String arrival, String destination) {
         if (departure.equals("") && arrival.equals("")) {
-            return this.iFlightScheduleAirCraftRepository.searchTicket(airPort, arrival, destination);
+            return this.iFlightScheduleAirCraftRepository.searchTicket(airPort, arrival,destination);
         } else if (!arrival.equals("") && departure.equals("")) {
-            return this.iFlightScheduleAirCraftRepository.searchTicket1(airPort, arrival, destination);
-        } else if (arrival.equals("")&&!departure.equals("")){
+
+          return this.iFlightScheduleAirCraftRepository.searchTicket1(airPort,arrival,destination);
+        } else if (arrival.equals("") && !departure.equals("")) {
+            Date date = new Date();
+            int day = date.getDate();
+            int a= Integer.parseInt(departure.substring(8, 10));
+            int month=date.getMonth()+1;
+            int year=date.getYear()+1900;
+            if (a <day &&Integer.parseInt(departure.substring(5,7))==month&&departure.substring(0,4).equals(String.valueOf(year))){
+                return this.iFlightScheduleAirCraftRepository.searchTicket4(airPort, arrival, destination);
+            }
             return this.iFlightScheduleAirCraftRepository.searchTicket2(airPort, departure, arrival, destination);
-        }else {
+        } else {
             return this.iFlightScheduleAirCraftRepository.searchTicket3(airPort, departure, arrival, destination);
         }
     }
